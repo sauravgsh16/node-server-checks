92ab3b37a58da6b04ca33e5d04125931ff8d34e1
Ghosh Saurav <GhoshSaurav@JohnDeere.com>
2018-05-30 21:08:40 +0530

    Added CLI logic

--- index.js
+++ index.js
@@ -5,16 +5,24 @@
 
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app
 
 var app = {};
 
 app.init = function(){
+
     // Start the server
     server.init();
+
     // Start the worker
     workers.init();
+
+    // Start the CLI, but make sure that it starts last
+    setTimeout(() => {
+        cli.init();
+    }, 50);
 };
 
 // Execute
